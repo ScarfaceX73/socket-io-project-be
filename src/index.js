285fa35638a33ff39eac517a246d5f5f514f4fca
Srinivasa Durai <kaizel73@gmail.com>
2023-03-15 11:48:48 +0530

    first

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origins: "*",
+        origins: false,
         methods: ["GET", "POST"],
     },
 });
