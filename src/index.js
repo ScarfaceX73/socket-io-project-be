44f10c233d35d70ac0af39945e7b95e11b039365
Srinivasa Durai <kaizel73@gmail.com>
2023-03-15 10:25:33 +0530

    first

--- src/index.js
+++ src/index.js
@@ -10,13 +10,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origins: "https://63419693434e17536c4657f9--startling-croquembouche-2f8126.netlify.app/",
+        origins: "https://master--startling-croquembouche-2f8126.netlify.app",
         methods: ["GET", "POST"],
     },
 });
 
 io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
+
     socket.on('join_room', (data) => {
         socket.join(data);
         console.log(`User with ID: ${socket.id} joined room: ${data}`);
@@ -25,11 +26,12 @@ io.on("connection", (socket) => {
     socket.on("send_message", (data) => {
         socket.to(data.room).emit("receive_message", data);
     })
+
     socket.on('disconnect', () => {
         console.log(`User disconnected: ${socket.id}`);
     });
 })
 
-server.listen(3001, () => {
-    console.log("SERVER RUNNING");
+server.listen(4000, () => {
+    console.log("SERVER RUNNING in PORT 4000");
 });
