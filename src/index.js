d5e4b6cdfab75b506d36d9b5808642758cb08d2d
Srinivasa Durai <kaizel73@gmail.com>
2023-03-15 11:42:23 +0530

    first

--- src/index.js
+++ src/index.js
@@ -8,26 +8,21 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-    cors: {
-        origins: "https://master--startling-croquembouche-2f8126.netlify.app/",
-        methods: ["GET", "POST"],
-    },
-});
+const io = new Server(server);
 
-// app.use((req, res, next) => {
-//     let allowedOrigin = ["http://localhost:3000", "https://master--startling-croquembouche-2f8126.netlify.app/"]
-//     if (allowedOrigin.indexOf(req.headers.origin) != -1) {
-//         res.header("Access-Control-Allow-Origin", req.headers.origin);
-//         res.setHeader("Access-Control-Allow-Credentials", true);
-//         res.setHeader("Access-Control-Allow-Headers", "Content-Type, Set-Cookie");
-//         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     }
+app.use((req, res, next) => {
+    let allowedOrigin = ["http://localhost:3000", "https://master--startling-croquembouche-2f8126.netlify.app/"]
+    if (allowedOrigin.indexOf(req.headers.origin) != -1) {
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+        res.setHeader("Access-Control-Allow-Credentials", true);
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Set-Cookie");
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    }
 
-//     console.log(allowedOrigin.indexOf(req.headers.origin));
+    console.log(allowedOrigin.indexOf(req.headers.origin));
 
-//     next();
-// })
+    next();
+})
 
 io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
